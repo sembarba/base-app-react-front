9e7d30c0c069520ec9ea4d0a916c1223e6333440
sem barba <133045021+sembarba@users.noreply.github.com>
2024-08-29 11:08:03 -0600

    Se agregaron iconos y estilos

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import ProtectedRoute from './routes/ProtectedRoute.js';
+import InicioAdmin from './routes/admin/InicioAdmin.js';
+import Login from './routes/Login.js';
+
+import { AuthProvider } from './context/AuthContext.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider> 
+        <Routes>
+          <Route path = "/*" element = {<Login />} />
+          {/****************** ROUTES DEL ADMIN *******************/}
+          <Route path = '/admin' element = {
+            <ProtectedRoute
+              acceso = "Admin"
+            >
+              <InicioAdmin />
+            </ProtectedRoute>
+          }>
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
